72ccf7ce124d09847104f9640cd28842d95397f8
Marino <marinobarroso2000@gmail.com>
2026-03-18 03:45:20 -0400

    Dependencias Base

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <main class="p-6">
+    <fwb-alert type="success">
+      Vue 3 + Flowbite
+    </fwb-alert>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { FwbAlert } from 'flowbite-vue'
+</script>
